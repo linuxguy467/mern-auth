40bd2042e6cea8d442d73415cab3bc964badc71c
linuxguy467 <me@matthem.codes>
2023-08-26 15:40:28 -0400

    Setup mongodb connection

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,12 @@ import express from 'express'
 import dotenv from 'dotenv'
 import userRoutes from './routes/userRoutes.js'
 import { errorHandler, notFound } from './middleware/errorMiddleware.js'
+import connectDB from './config/db.js'
 
 dotenv.config()
 const port = process.env.PORT || 5001
 
+connectDB()
 const app = express()
 
 app.use('/api/users', userRoutes)
