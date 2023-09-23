e41c94388b81803eb4bb222c2899e9a3a42037db
Matthew Hemingway <dpplrdr435@gmail.com>
2023-09-23 15:47:25 +0000

    setup git config and mongo

---
+++ .gitpod/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -euo pipefail
+
+git config --global user.name "Matthew Hemingway"
+git config --global user.email dpplrdr435@gmail.com
+git config --global core.editor vim
+git config --global core.autocrlf input
+
+docker compose up -d
+sleep 50
+
+touch .env
+echo "MONGO_URI=mongodb://localAdmin:mongolocal@localhost:27017/test" >> .env
