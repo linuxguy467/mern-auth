4537549e1e9612069fc9ab963cad859c314d541a
linuxguy467 <me@matthem.codes>
2023-09-30 11:25:45 -0400

    add user auth functionality

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -31,6 +31,10 @@ userSchema.pre('save', async function (next) {
   this.password = await bcrypt.hash(this.password, salt)
 })
 
+userSchema.methods.matchPassword = async function (password) {
+  return await bcrypt.compare(password, this.password)
+}
+
 const User = mongoose.model('User', userSchema)
 
 export default User
