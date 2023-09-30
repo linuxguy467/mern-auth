3cfb93217e755d3ace6390e78fe3ddebf66bb08c
linuxguy467 <me@matthem.codes>
2023-09-30 13:12:14 -0400

    get cookie-parser lib; add auth middleware; add get user profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -57,21 +57,26 @@ const registerUser = asyncHandler(async (req, res) => {
 
 // @desc    Logout user
 // route    POST /api/users/logout
-// @access  Public
+// @access  Private
 const logoutUser = asyncHandler(async (req, res) => {
   res.cookie('jwt', '', {
     httpOnly: true,
     expires: new Date(0),
   })
 
-  res.status(200).json({ message: 'User logout User' })
+  res.status(200).json({ message: 'User logged out' })
 })
 
 // @desc    Get user profile
 // route    GET /api/users/profile
 // @access  Private
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'User profile' })
+  const user = {
+    _id: req.user._id,
+    name: req.user.name,
+    email: req.user.email,
+  }
+  res.status(200).json(user)
 })
 
 // @desc    Update user profile
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import dotenv from 'dotenv'
+dotenv.config()
+import cookieParser from 'cookie-parser'
 import userRoutes from './routes/userRoutes.js'
 import { errorHandler, notFound } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 
-dotenv.config()
 const port = process.env.PORT || 5000
 
 connectDB()
@@ -12,6 +13,8 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+app.use(cookieParser())
 
 app.use('/api/users', userRoutes)
 
