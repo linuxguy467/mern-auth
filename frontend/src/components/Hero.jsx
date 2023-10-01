04b741e276e57b0f282cc97c2613e787895751bc
linuxguy467 <me@matthem.codes>
2023-09-30 20:31:49 -0400

    add homescreen and hero page

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,26 @@
+import { Button, Card, Container } from 'react-bootstrap'
+
+const Hero = () => {
+  return (
+    <div className='py-5'>
+      <Container className='d-flex justify-content-center'>
+        <Card className='p-5 d-flex flex-column align-items-center hero-card bg-light w-75'>
+          <h1 className='text-center mb-4'>MERN Authentication</h1>
+          <p className='text-center mb-4'>
+            This is a boilerplate for MERN authentication that stores a JWT in an HTTP-Only cookie. <br />
+            It also uses Redux Toolkit and the React Bootstrap library.
+          </p>
+          <div className='d-flex'>
+            <Button variant='primary' href='/login' className='me-3'>
+              Sign In
+            </Button>
+            <Button variant='secondary' href='/register'>
+              Sign Up
+            </Button>
+          </div>
+        </Card>
+      </Container>
+    </div>
+  )
+}
+export default Hero
