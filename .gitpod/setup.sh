1869c1d3f97e282fbf4f6ee1b8676b2359f13733
Matthew Hemingway <dpplrdr435@gmail.com>
2023-10-01 15:50:03 +0000

    fix mongo uri in setup script and use gitpod vscode theme

--- .gitpod/setup.sh
+++ .gitpod/setup.sh
@@ -25,7 +25,7 @@ cd $WORKSPACE_PATH/
 cp .env.example .env
 sed -Ei 's/NODE_ENV=.*/NODE_ENV=development/' .env
 sed -Ei 's/PORT=.*/PORT=5000/' .env
-sed -Ei 's/MONGO_URI=.*/MONGO_URI=mongodb:\/\/mernauthapp:mernauthpass@localhost:27017\/\?authSource=admin\&readPreference=primary\&ssl=false\&directConnection=true/' .env
+sed -Ei 's/MONGO_URI=.*/MONGO_URI=mongodb:\/\/mernauthapp:mernauthpass@localhost:27017\/\mernauth?authSource=admin\&readPreference=primary\&ssl=false\&directConnection=true/' .env
 sed -Ei 's/JWT_SECRET=.*/JWT_SECRET=abc123/' .env
 
 # Frontend environments
